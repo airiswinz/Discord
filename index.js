bd3b415715d24d70e7c791d5c4e75bbbbc9c0456
airiswinz <165577465+airiswinz@users.noreply.github.com>
2024-04-01 00:48:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -23,28 +23,28 @@ client.on('ready', async () => {
   console.log(`${client.user.tag} - rich presence started!`);
 
   const r = new Discord.RichPresence()
-    .setApplicationId('')
+    .setApplicationId('1200448965259300874')
     .setType('STREAMING')
-    .setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ') //Must be a youtube video link 
-    .setState('Recording')
-    .setName('')
+    .setURL('https://www.youtube.com/watch?v=4YbnyLqSLbw') //Must be a youtube video link 
+    .setState('Valorant')
+    .setName('ris')
     .setDetails(`Valorant [${formatTime()}]`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('Boring') //Text when you hover the Large image
-    .setAssetsSmallImage('') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('Twitch') //Text when you hover the Small image
-    .addButton('Watch', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-    .addButton('Donate', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+ .setAssetsLargeImage('https://i.pinimg.com/564x/d1/92/f4/d192f4e8c7c281ae9a7bd850b5f99164.jpg') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('prems, game creds & nitro boosts') //Text when you hover the Large image
+    .setAssetsSmallImage('https://media.discordapp.net/attachments/1167525454308180030/1224036299703455944/green_check.gif?ex=661c07a3&is=660992a3&hm=2c67683c6fa44f018ebe0d42b811f0ec74490b0ddefe31e93588cecb88fb512e&=&width=58&height=59') //You can put links in tenor or discord and etc.
+    .setAssetsSmallText('legit shop') //Text when you hover the Small image
+    .addButton('Shop ‹𝟥', ' https://discord.gg/JTnVN2NJhq')
+    .addButton('Vouches ‹𝟥', 'https://discord.gg/Yjg2KpWryS');
 
   client.user.setActivity(r);
-  client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
+  client.user.setPresence({ status: "idle" }); //dnd, online, idle, offline
 
   let prevTime = null;
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = ` [${newTime}]`;
+      const newDetails = `ris ૮ ˶ᵔ ᵕ ᵔ˶ ა `;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
